37c55220ce104ac68dea5879559f8d83d948098b
xtomash <thehejik@koukaam.se>
2014-11-14 13:20:57 +0100

    Print all variables and its values to the user log

--- print_all_variables.lua
+++ print_all_variables.lua
@@ -2,18 +2,18 @@
 
 logf("-----")
 local function toDebugString(a, indent)
-		if type(a) == 'table' then
-			indent = indent or 0
-			local out = {'table\n'}
-			for k, v in pairs(a) do
-				table.insert(out, string.format('%s%s -> %s\n', string.rep('   ', indent), tostring(k), toDebugString(v, indent+1)))
-			end
-			return table.concat(out)
-		else
-			return tostring(a)
-		end
+  if type(a) == 'table' then
+    indent = indent or 0
+    local out = {'table\n'}
+    for k, v in pairs(a) do
+      table.insert(out, string.format('%s%s -> %s\n', string.rep('   ', indent), tostring(k), toDebugString(v, indent+1)))
+    end
+    return table.concat(out)
+  else
+    return tostring(a)
+  end
 end 
 
 for key, item in pairs(devices) do
-      logf("%s -> %s", key, toDebugString(item, 0))
+  logf("%s -> %s", key, toDebugString(item, 0))
 end
